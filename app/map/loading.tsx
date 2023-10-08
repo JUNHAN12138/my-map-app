0db4f4ddd5142306260d3fbbeae14a9ecfda386d
liangmingle <2206821639@qq.com>
2023-10-08 22:05:29 +0800

    feat: 添加loading

---
+++ app/map/loading.tsx
@@ -0,0 +1,16 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      loading
+    </div>
+  );
+}
